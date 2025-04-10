8e337a679e77c0fc958eeb264e8341619229fae8
George Hyde <110679625+georgehyde-dot@users.noreply.github.com>
2025-04-10 18:28:46 -0500

    initial expressions and prefixes (#12)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gosling
 A simple language to learn how they work
+Go buy Thorsten Ball's [Book](https://interpreterbook.com/) which this is based on. I'm following it, but trying to add extra features and make my own changes along the way.  
--- ast/ast.go
+++ ast/ast.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Node interface {
-	Tokenliteral() string
+	TokenLiteral() string
 	String() string
 }
 
@@ -48,7 +48,7 @@ type ExpressionStatement struct {
 // Program methods
 func (p *Program) Tokenliteral() string {
 	if len(p.Statements) > 0 {
-		return p.Statements[0].Tokenliteral()
+		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
@@ -66,16 +66,16 @@ func (p *Program) String() string {
 
 // Identifier methods
 func (i *Identifier) expressionNode()      {}
-func (i *Identifier) Tokenliteral() string { return i.Token.Literal }
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
 // LetStatement methods
 func (ls *LetStatement) statementNode()       {}
-func (ls *LetStatement) Tokenliteral() string { return ls.Token.Literal }
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 func (ls *LetStatement) String() string {
 	var out bytes.Buffer
 
-	out.WriteString(ls.Tokenliteral() + " ")
+	out.WriteString(ls.TokenLiteral() + " ")
 	out.WriteString(ls.Name.String())
 	out.WriteString(" = ")
 
@@ -90,11 +90,11 @@ func (ls *LetStatement) String() string {
 
 // ReturnStatement methods
 func (rs *ReturnStatement) statementNode()       {}
-func (rs *ReturnStatement) Tokenliteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 func (rs *ReturnStatement) String() string {
 	var out bytes.Buffer
 
-	out.WriteString(rs.Tokenliteral() + " ")
+	out.WriteString(rs.TokenLiteral() + " ")
 
 	if rs.ReturnValue != nil {
 		out.WriteString(rs.ReturnValue.String())
@@ -106,7 +106,7 @@ func (rs *ReturnStatement) String() string {
 }
 
 // ExpressionStatement methods
-// func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 func (es *ExpressionStatement) String() string {
 	if es.Expression != nil {
