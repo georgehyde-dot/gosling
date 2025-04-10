d07fdde8df9b3709fbf6b9d38638fbb049c23261
George Hyde <georgewhyde3@gmail.com>
2025-04-10 18:19:59 -0500

    initial expressions and prefixes

--- parser/parser.go
+++ parser/parser.go
@@ -12,15 +12,47 @@ type Parser struct {
 	curToken  token.Token
 	peekToken token.Token
 	errors    []string
+
+	prefixParseFns map[token.TokenType]prefixParseFns
+	infixParseFns  map[token.TokenType]infixParseFns
 }
+
+// look more into how this works
+type (
+	prefixParseFns func() ast.Expression
+	infixParseFns  func(ast.Expression) ast.Expression
+)
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // < or >
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -x or !x
+	CALL        // func(x)
+)
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFns)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.infixParseFns = make(map[token.TokenType]infixParseFns)
 
 	p.nextToken()
 	p.nextToken()
 	return p
 }
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFns) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+// func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFns) {
+// 	p.infixParseFns[tokenType] = fn
+// }
 
 func (p *Parser) Errors() []string {
 	return p.errors
@@ -62,7 +94,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -100,15 +132,39 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
 func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
 	for p.curToken.Type != token.EOF {
 		stmt := p.parseStatement()
-		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
-		}
+		program.Statements = append(program.Statements, stmt)
 		p.nextToken()
 	}
 	return program
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -49,8 +49,8 @@ func TestLetStatements(t *testing.T) {
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
-	if s.Tokenliteral() != "let" {
-		t.Errorf("s.TokenLiteral not 'let', got %q", s.Tokenliteral())
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let', got %q", s.TokenLiteral())
 	}
 
 	letStmt, ok := s.(*ast.LetStatement)
@@ -64,8 +64,8 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 
-	if letStmt.Name.Tokenliteral() != name {
-		t.Errorf("letStmt.Name.TokenLiteral not %s, got=%s", name, letStmt.Name.Tokenliteral())
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral not %s, got=%s", name, letStmt.Name.TokenLiteral())
 		return false
 	}
 	return true
@@ -88,8 +88,44 @@ func TestResturnStatements(t *testing.T) {
 			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
 			continue
 		}
-		if returnStmt.Tokenliteral() != "return" {
-			t.Errorf("returnStmt.TokenLiteral not 'return', got=%q", returnStmt.Tokenliteral())
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got=%q", returnStmt.TokenLiteral())
 		}
+	}
+}
+
+func TestIdentifierExpression(t *testing.T) {
+	inputFile := "testidentexp.gos"
+
+	l := lexer.LexFile(inputFile)
+
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program does not have the correct # of statements, got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("stmt.Expression is not *ast.Identifier. got=%T",
+			stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Fatalf("ident.Value is not %s. got=%T",
+			"foobar", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral is not %s, got=%s",
+			"foobar", ident.TokenLiteral())
 	}
 }
