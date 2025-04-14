8be68c0b5d11430da001e65f5c15f5d121667da9
George Hyde <110679625+georgehyde-dot@users.noreply.github.com>
2025-04-14 08:10:22 -0500

    add int literal expressions (#14)

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"gosling/ast"
 	"gosling/lexer"
 	"gosling/token"
+	"strconv"
 )
 
 type Parser struct {
@@ -39,6 +40,8 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFns)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+
 	p.infixParseFns = make(map[token.TokenType]infixParseFns)
 
 	p.nextToken()
@@ -156,6 +159,20 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
