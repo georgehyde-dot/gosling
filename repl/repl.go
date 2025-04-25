a7b599926ef14c5e133f63845b27f42e74d15695
George Hyde <georgewhyde3@gmail.com>
2025-04-24 20:35:35 -0500

    finish initial parser, repl shows parsed strings

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"gosling/lexer"
+	"gosling/parser"
 	"gosling/token"
 	"io"
 )
@@ -21,11 +22,35 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		fmt.Fprintln(out, line)
 		l := lexer.LexRepl(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		n, err := io.WriteString(out, program.String())
+		if err != nil {
+			fmt.Printf("error writing to out, %d bytes\n", n)
+		}
+		n, err = io.WriteString(out, "\n")
+		if err != nil {
+			fmt.Printf("error writing to out, %d bytes\n", n)
+		}
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		n, err := io.WriteString(out, "\t"+msg+"\n")
+		if err != nil {
+			fmt.Printf("error writing to out, %d bytes\n", n)
 		}
 	}
 }
