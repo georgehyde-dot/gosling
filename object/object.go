1d0c246e4c3d71a5adc5d4437439ce799fe008ad
George Hyde <110679625+georgehyde-dot@users.noreply.github.com>
2025-04-29 20:59:57 -0500

    initial evaluator (#26)

---
+++ object/object.go
@@ -0,0 +1,43 @@
+package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	ERROR_OBJ   = "ERROR"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+type Boolean struct {
+	Value bool
+}
+
+type Error struct {
+	Value error
+}
+
+// Integer Methods
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+// Boolean Methods
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+// I'm not doing NULL values, but I will include and error return
+// Error Methods
+func (e *Error) Inspect() error   { return fmt.Errorf("%w", e.Value) }
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
